431f1ce9bae306a7eb40bcc8ccca97e986d106f8
Sachin KM <hiscan99@gmail.com>
2025-08-07 15:49:10 +0200

    feat: add SiteFooter component to enhance layout in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { TimeTracker } from "./components/TimeTracker";
 import SiteHeader from "./components/SiteHeader";
+import SiteFooter from "./components/SiteFooter";
 function App() {
   return (
     <>
       <SiteHeader />
       <TimeTracker />
+      <SiteFooter />
     </>
   );
 }
