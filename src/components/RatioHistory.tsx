a52448e1b062198f5459ba1309ec77b1fd38e799
Sachin KM <hiscan99@gmail.com>
2025-09-17 13:02:29 +0200

    feat: add date-fns and react-router dependencies; implement RatioHistory component

---
+++ src/components/RatioHistory.tsx
@@ -0,0 +1,10 @@
+const RatioHistory = () => {
+  return (
+    <div className="bg-white p-4 m-4 rounded shadow min-h-[75vh]">
+      RatioHistory
+      <p>Here you can see the history of your stand/sit ratios.</p>
+    </div>
+  );
+};
+
+export default RatioHistory;
